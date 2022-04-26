c4a8695beae8016f8cb61f4de3a2dbb1677c1d29
AbhirathNB <f20200260@hyderabad.bits-pilani.ac.in>
2022-04-26 09:47:35 +0530

    update 1

---
+++ Hello/home/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpRequest
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("this is homepage")
+
+def about(request):
+    return HttpResponse("this is aboutpage")
